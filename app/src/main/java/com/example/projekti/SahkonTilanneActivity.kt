3b42b7d2da329970abe3641ad2dc27dd364dab73
EriKorh <t1koer00@students.oamk.fi>
2023-03-22 11:09:24 +0200

    Luotu tarvittavat aktiviteetit

---
+++ app/src/main/java/com/example/projekti/SahkonTilanneActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projekti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SahkonTilanneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sahkon_tilanne)
+    }
+}
