dc086fe4684dd82022bc58c1bbc5ea3a3e7a6298
Onni Hietanen <hietanne@gmail.com>
2023-03-24 14:33:37 +0200

    Muokattu koodia

--- app/src/main/java/com/example/projekti/MainActivity.kt
+++ app/src/main/java/com/example/projekti/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projekti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
