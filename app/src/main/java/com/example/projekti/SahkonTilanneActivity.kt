35e2b6669a53492fc3134795fdf9a93953fc3b9f
EriKorh <t1koer00@students.oamk.fi>
2023-03-24 18:17:37 +0200

    Sivut avautuvat oikeista napeista. Puuttuu vielä kirjautuminen

--- app/src/main/java/com/example/projekti/SahkonTilanneActivity.kt
+++ app/src/main/java/com/example/projekti/SahkonTilanneActivity.kt
@@ -1,11 +1,25 @@
 package com.example.projekti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SahkonTilanneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sahkon_tilanne)
+        val nextButton:Button = findViewById(R.id.kulutusarvioButton)
+        val settingsButton:Button = findViewById(R.id.settingsButton)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, OmaKulutusarvioActivity::class.java);
+            startActivity(intent);
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
