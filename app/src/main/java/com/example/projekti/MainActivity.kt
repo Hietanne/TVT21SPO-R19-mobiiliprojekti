d3b8ec1ebd7dbf879bd796eeee620703e4a55315
jan5u <janhenrik02@hotmail.fi>
2023-03-26 11:44:57 +0300

    Tumma teema nappula toimii jotenki

--- app/src/main/java/com/example/projekti/MainActivity.kt
+++ app/src/main/java/com/example/projekti/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.projekti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private fun handleTheme() {
+        val sharedPreferences = getSharedPreferences("myPreferences", MODE_PRIVATE)
+        val isDarkModeOn = sharedPreferences.getBoolean("darkMode", false)
+        if (isDarkModeOn) {
+            setTheme(R.style.Theme_Projekti_Dark)
+
+        } else {
+            setTheme(R.style.Theme_Projekti)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        handleTheme()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
--- app/src/main/java/com/example/projekti/SettingsActivity.kt
+++ app/src/main/java/com/example/projekti/SettingsActivity.kt
@@ -3,16 +3,37 @@ package com.example.projekti
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.widget.Button
+import androidx.appcompat.widget.SwitchCompat
+import android.content.SharedPreferences
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("myPreferences", MODE_PRIVATE)
+
+        val isDarkModeEnabled = sharedPreferences.getBoolean("darkMode", false)
+
+
+        setTheme(if (isDarkModeEnabled) R.style.Theme_Projekti_Dark else R.style.Theme_Projekti)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val backButton:Button = findViewById(R.id.backButton)
 
         backButton.setOnClickListener {
             onBackPressed()
         }
+
+        val darkModeButton: SwitchCompat = findViewById(R.id.darkModeButton)
+        darkModeButton.isChecked = isDarkModeEnabled
+
+        darkModeButton.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("darkMode", isChecked).apply()
+            recreate()
+        }
+
+
     }
 }
